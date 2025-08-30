346190955c2371be6a966e7e1e44eee5d1c23f7b
ajay2512 <bhakarajay033@gmail.com>
2025-08-30 12:09:59 +0530

    FIx env issue

--- backend/app.js
+++ backend/app.js
@@ -20,3 +20,8 @@ app.use("/projects", projectRoutes);
 app.use("/search", searchRoutes);
 
 export default app;
+
+
+SUPABASE_URL=https://qggvnrigidnvnxwpjsid.supabase.co
+SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnZ3ZucmlnaWRudm54d3Bqc2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0ODgwOTcsImV4cCI6MjA3MjA2NDA5N30.BFezzUx_9NL-m8s5HjnpqhvxEIqP3AByJNSSjxcFUR0
+PORT=5000
